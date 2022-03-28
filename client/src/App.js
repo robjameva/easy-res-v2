257dc97a4969799e40a20d5e2063d005ef04e44c
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-03-27 20:19:03 -0400

    home layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+// import { Route, BrowserRouter, Routes } from "react-router-dom";
+// import Dashboard  from "";
+// import Login from "";
+// import Signup from "";
+import Home from "../src/components/Home";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className='contact-list'>
+//         <Routes>
+//           <Route path="/" element={<Home/>} />
+//           {/* <Route path="/dashboard/:id" element={<Dashboard/>} />
+//           <Route path="login" element={<Login/>} />
+//           <Route path="/signup" element={<Signup/>} /> */}
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+export default function App() {
+  return(
+    <div>
+      <Home/>
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <section className='hero'>
+           <h1>Make Your Reservation Today</h1>
+        </section>
+    );
+}
+
+export default Hero;
