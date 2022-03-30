62ecd29bbf36d381955c01ffa8ee911f73581807
Sean Gillespie <seene3@gmail.com>
2022-03-29 21:10:12 -0400

    fixing merge conflict

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-// use this to decode a token and get the user's information out of it
-import decode from 'jwt-decode';
-
-// create a new class to instantiate for a user
-class AuthService {
-  // get user data
-  getProfile() {
-    return decode(this.getToken());
-  }
-
-  // check if user's logged in
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-
-  // check if token is expired
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
-    }
-  }
-
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
-  }
-
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
-  }
-}
-
-export default new AuthService();
-=======
 import decode from 'jwt-decode';
 
 class AuthService {
@@ -103,4 +52,3 @@ class AuthService {
 }
 
 export default new AuthService();
->>>>>>> 8e3dc4f10ecbea5e56d6593ba1450b2c6e58f56d
