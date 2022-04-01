dfa256a18dffa4e7108839d9414756be2fc673a0
Robert Evanik <rob.evanik@gmail.com>
2022-04-01 11:55:12 -0700

    Added SMS functionality and commentted out logic to not send SMS until we are ready

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Restaurant, Reservation } = require('../models');
 const { signToken } = require('../utils/auth');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const { format_business_hours } = require('../utils/helpers');
 
 const resolvers = {
     Query: {
@@ -55,6 +60,22 @@ const resolvers = {
         },
         createReservation: async (parent, { input }) => {
             const reservation = await Reservation.create(input);
+            const newReservation = await Reservation.find({ _id: reservation._id }).populate('user').populate('restaurant');
+
+            const businessName = newReservation[0].restaurant.business_name;
+            const party = newReservation[0].party_size;
+            const timeSlot = format_business_hours([newReservation[0].time_slot])[0];
+            const firstName = newReservation[0].user.first_name;
+            const phoneNumber = newReservation[0].user.phone_number;
+
+
+            // client.messages
+            //     .create({
+            //         body:
+            //             `Hello ${firstName}, your table is confirmed at ${businessName} for ${party} people at ${timeSlot}.`,
+            //         from: '+17853776055',
+            //         to: `+1${phoneNumber}`
+            //     })
 
             return reservation;
         },
