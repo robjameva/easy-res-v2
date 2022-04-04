9f97efebbae0405e4299e75ec805a538ca6d9bb8
cdm83092 <chrismack135@gmail.com>
2022-04-04 10:44:49 -0400

    alice carousel

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_RESTAURANTS } from '../../utils/queries'
+import { Link } from 'react-router-dom';
+import Carousel from "react-material-ui-carousel";
+import autoBind from 'react-autobind';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
@@ -12,169 +17,85 @@ import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 import CardActions from '@mui/material/CardActions';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import Carousel from 'react-material-ui-carousel'
-import { useQuery } from "@apollo/client";
-import { GET_ALL_RESTAURANTS } from '../../utils/queries'
-import { Link } from 'react-router-dom';
-import autoBind from 'react-autobind';
+import ReactDOM from "react-dom";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const handleOnDragStart = e => e.preventDefault();
 
 
-function Banner(props) {
-  // const totalItems = props.length ? props.length : 3;
 
-  if (loading.length) <h1>Loading</h1>
-  console.log(data)
-
+export default function CarouselHome() {
   const { loading, error, data } = useQuery(GET_ALL_RESTAURANTS);
 
   const restaurantData = data?.getAllRestaurants || [];
 
-  let items = [];
+  const items = restaurantData;
 
-  for ( let i = 0; i < restaurantData.length; i++) {
-    const item = restaurantData[i];
-    const media = (
-      <Grid item xs={12/3 } key={i}>
-        <Link to={`/restaurant/${item._id}`}>
-        <Card sx={{ height: '100%'}}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            ER
-          </Avatar>
-        }
-        title={item.business_name}
-        subheader="Casual Dining"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={item.Image}
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-        {item.business_address}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        <Typography variant="body2" color="text.secondary">
-          {item.business_website}
-          </Typography>
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="call">
-          <PhoneIcon />
-        </IconButton>
-      </CardActions>
-         
-    </Card>
-    </Link>
-      </Grid>
-    );
+  if (loading.length) <h1>Loading</h1>
+  console.log(data)
 
-    items.push(media);
-      }
-
- 
   return (
-    <Card raised className="Banner">
-      <Grid container spacing={0} className="BannerGrid">
-        {items}
-      </Grid>
-    </Card>
+    <div className="App">
+      <AliceCarousel
+        responsive={{
+          0: {
+            items: 1
+          },
+          700: {
+            items: 2
+          },
+          1024: {
+            items: 3
+          },
+          1300: {
+            items: 4
+          }
+        }}
+        mouseDragEnabled
+      >
+        {items.map((i) => {
+          console.log(i)
+          return (
+            <Card sx={{ height: '100%' }} key={i}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    ER
+                  </Avatar>
+                }
+                title={items.business_name}
+                subheader="Casual Dining"
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                image={items.image}
+                alt="Paella dish"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {items.business_address}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {items.business_website}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="call">
+                  <PhoneIcon />
+                </IconButton>
+              </CardActions>
+
+            </Card>
+          )
+        })
+        }
+      </AliceCarousel>
+    </div>
   );
 }
-const items = [1,2,3]
 
-class CarouselHome extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      autoPlay: true,
-      animation: "fade",
-      indicators: true,
-      timeout: 500,
-      navButtonsAlwaysVisible: false,
-      navButtonsAlwaysInvisible: false,
-      cycleNavigation: true
-    };
-
-    autoBind(this);
-  }
-
-  toggleAutoPlay() {
-    this.setState({
-      autoPlay: !this.state.autoPlay
-    });
-  }
-
-  toggleIndicators() {
-    this.setState({
-      indicators: !this.state.indicators
-    });
-  }
-
-  toggleNavButtonsAlwaysVisible() {
-    this.setState({
-      navButtonsAlwaysVisible: !this.state.navButtonsAlwaysVisible
-    });
-  }
-
-  toggleNavButtonsAlwaysInvisible() {
-    this.setState({
-      navButtonsAlwaysInvisible: !this.state.navButtonsAlwaysInvisible
-    });
-  }
-
-  toggleCycleNavigation() {
-    this.setState({
-      cycleNavigation: !this.state.cycleNavigation
-    });
-  }
-
-  changeAnimation(event) {
-    this.setState({
-      animation: event.target.value
-    });
-  }
-
-  changeTimeout(event, value) {
-    this.setState({
-      timeout: value
-    });
-  }
-
-  render() {
-    return (
-      <div style={{ paddingTop:"35px", marginTop: "0px", color: "#494949" }}>
-
-        <Carousel style={{display: 'flex', justify_content: 'center'}}
-          className="Example"
-          autoPlay={this.state.autoPlay}
-          animation={this.state.animation}
-          indicators={this.state.indicators}
-          timeout={this.state.timeout}
-          cycleNavigation={this.state.cycleNavigation}
-          navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
-          navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
-        >
-          {items.map((item, index) => {
-            return (
-              <Banner
-                item={item}
-                key={index}
-              />
-            );
-          })}
-        </Carousel>
-      </div>
-    );
-  }
-}
-
-export default CarouselHome;
