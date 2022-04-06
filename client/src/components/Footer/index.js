cc5a33650033b049527be4e2c300a59cb19a1108
Sean Gillespie <seene3@gmail.com>
2022-04-05 20:23:52 -0400

    added edit user button to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,21 +2,25 @@
    
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Easy Res
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <>
+      
+      <Typography variant="body2" color="text.secondary">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Easy Res
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </>
   );
 }
 
@@ -31,9 +35,13 @@ export default function Footer() {
         }}
         style={{backgroundColor:'white'}}
       >
+        
         <Container width="fullWidth" 
         style={{display: 'flex', justifyContent: 'flex-end'}}>
           <Copyright />
+          <Link to="/user-dashboard" style={{ textDecoration: 'none' }}>
+              <Button size="medium" variant="outlined" style={{ color: '#21325e' }}>Edit User</Button>
+          </Link>
         </Container>
       </Box>
   );
