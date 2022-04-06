068246cfa5f831e6579939211b6201ac2d3f8897
Nicholas Defex <ndefected15@gmail.com>
2022-04-06 19:22:55 -0400

    side bar conditionally rendering sign-in/sign-out

--- client/src/components/AppBar/index.js
+++ client/src/components/AppBar/index.js
@@ -20,196 +20,271 @@ import Home from '@mui/icons-material/Home';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import EmailIcon from '@mui/icons-material/Email';
 import PersonIcon from '@mui/icons-material/Person';
-import logo from '../../assets/images/logo.png'
-import { Link } from "react-router-dom";
+import logo from '../../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import AddIcon from '@mui/icons-material/Add';
-import FoodBankIcon from '@mui/icons-material/FoodBank'
+import FoodBankIcon from '@mui/icons-material/FoodBank';
 
 const listItems = [
-  {
-    listIcon: <Home />,
-    listText: "Home",
-    link: '/'
-  },
-  {
-    listIcon: <DashboardIcon />,
-    listText: "Dashboard",
-    link: '/user-dashboard'
-  },
-  {
-    listIcon: <RestaurantIcon />,
-    listText: "Owned Restaurants",
-    link: '/owner-dashboard'
-  },
-  {
-    listIcon: <FoodBankIcon />,
-    listText: "Add New Restaurant",
-    link: '/owner/add-restaurant'
-  },
-  {
-    listIcon: <FoodBankIcon />,
-    listText: "SV",
-    link: '/restaurant/:restaurantId'
-  },
-  {
-    listIcon: <PersonIcon />,
-    listText: "Login",
-    link: '/login'
-  },
-  {
-    listIcon: <AddIcon />,
-    listText: "Sign Up",
-    link: '/sign-up'
-  }
+	{
+		listIcon: <Home />,
+		listText: 'Home',
+		link: '/',
+		loggedIn: 'true',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <DashboardIcon />,
+		listText: 'Dashboard',
+		link: '/user-dashboard',
+		loggedIn: 'true',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <RestaurantIcon />,
+		listText: 'Owned Restaurants',
+		link: '/owner-dashboard',
+		loggedIn: 'true',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <FoodBankIcon />,
+		listText: 'Add New Restaurant',
+		link: '/owner/add-restaurant',
+		loggedIn: 'true',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <PersonIcon />,
+		listText: 'Login',
+		link: '/login',
+		loggedIn: 'false',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <AddIcon />,
+		listText: 'Sign Up',
+		link: '/sign-up',
+		loggedIn: 'false',
+		loggedOut: 'true',
+	},
+	{
+		listIcon: <AddIcon />,
+		listText: 'Log Out',
+		link: '/',
+		loggedIn: 'true',
+		loggedOut: 'false',
+	},
 ];
 
 const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	},
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+				width: '20ch',
+			},
+		},
+	},
 }));
 
-
-
 export default function SearchAppBar() {
+	const [open, setOpen] = useState(false);
 
-  const [open, setOpen] = useState(false);
+	const toggleSlider = () => {
+		setOpen(!open);
+	};
 
-  const toggleSlider = () => {
-    setOpen(!open);
-  };
+	const logout = (event) => {
+		event.preventDefault();
+		Auth.logout();
+	};
 
-  const logout = event => {
-    event.preventDefault();
-    Auth.logout();
-  };
+	const SideList = () => (
+		<Box
+			sx={{
+				width: 250,
+				background: '#2f3136',
+				height: '100%',
+			}}
+			component="div"
+		>
+			<Divider />
 
-  const SideList = () => (
-    <Box sx={{
-      width: 250,
-      background: "#2f3136",
-      height: "100%"
-    }} component="div">
-      <Divider />
-      <List>
-        {listItems.map((listItem, index) => (
-          <Link key={index} to={listItem.link}>
-            <ListItem onClick={toggleSlider} style={{ color: 'white' }} button >
-              <ListItemIcon style={{ color: 'white' }}>
-                {listItem.listIcon}
-              </ListItemIcon>
-              <ListItemText primary={listItem.listText} />
-            </ListItem>
-          </Link>
-        ))}
-        <Link to='#'
-          onClick={() => window.location = 'mailto:easyResFakeEmail@notRealEmail.org'}>
-          <ListItem onClick={toggleSlider} style={{ color: 'white' }}>
-            <ListItemIcon style={{ color: 'white' }}>
-              <EmailIcon />
-            </ListItemIcon>
-            <ListItemText primary='Contact' />
-          </ListItem>
-        </Link>
-      </List>
-    </Box>
-  );
+			<List>
+				{Auth.loggedIn() ? (
+					<>
+						{console.log(listItems)}
+						{listItems
+							.filter((loginList) => loginList.loggedIn === 'true')
+							.map((listItem, index) => (
+								<Link key={index} to={listItem.link}>
+									<ListItem
+										onClick={toggleSlider}
+										style={{ color: 'white' }}
+										button
+									>
+										<ListItemIcon style={{ color: 'white' }}>
+											{listItem.listIcon}
+										</ListItemIcon>
+										<ListItemText primary={listItem.listText} />
+									</ListItem>
+								</Link>
+							))}
+						<Link
+							to="#"
+							onClick={() =>
+								(window.location = 'mailto:easyResFakeEmail@notRealEmail.org')
+							}
+						>
+							<ListItem onClick={toggleSlider} style={{ color: 'white' }}>
+								<ListItemIcon style={{ color: 'white' }}>
+									<EmailIcon />
+								</ListItemIcon>
+								<ListItemText primary="Contact" />
+							</ListItem>
+						</Link>
+					</>
+				) : (
+					<>
+						{listItems
+							.filter((loginList) => loginList.loggedOut === 'true')
+							.map((listItem, index) => (
+								<Link key={index} to={listItem.link}>
+									<ListItem
+										onClick={toggleSlider}
+										style={{ color: 'white' }}
+										button
+									>
+										<ListItemIcon style={{ color: 'white' }}>
+											{listItem.listIcon}
+										</ListItemIcon>
+										<ListItemText primary={listItem.listText} />
+									</ListItem>
+								</Link>
+							))}
+						<Link
+							to="#"
+							onClick={() =>
+								(window.location = 'mailto:easyResFakeEmail@notRealEmail.org')
+							}
+						>
+							<ListItem onClick={toggleSlider} style={{ color: 'white' }}>
+								<ListItemIcon style={{ color: 'white' }}>
+									<EmailIcon />
+								</ListItemIcon>
+								<ListItemText primary="Contact" />
+							</ListItem>
+						</Link>
+					</>
+				)}
+			</List>
+		</Box>
+	);
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ background: '#FFFFFF' }}>
-        <Toolbar>
-          <IconButton onClick={toggleSlider}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 0 }}
-          >
-            <MenuIcon
-              style={{ color: '#21325e' }} />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-            style={{ color: '#21325e' }}
-          >
-            <Link to='/'>
-              <img className='logo' src={logo} alt="logo" />
-            </Link>
-            {/* Easy Res */}
-          </Typography>
-          <Drawer open={open} anchor="left" onClose={toggleSlider}>
-            {SideList()}
-          </Drawer>
-          <Stack direction="row" spacing={2}>
-            {Auth.loggedIn() ? (
-              <>
-                <a href="/" onClick={logout}>
-                  <Button className='signBtn' variant="outlined" style={{ color: '#21325e' }}>Logout</Button>
-                </a>
-              </>
-            ) : (
-              <>
-                <Link to="/login" style={{ textDecoration: 'none' }}>
-                  <Button size="medium" className='signBtn' variant="outlined" style={{ color: '#21325e' }}>Login</Button>
-                </Link>
-              </>
-            )}
-          </Stack>
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static" style={{ background: '#FFFFFF' }}>
+				<Toolbar>
+					<IconButton
+						onClick={toggleSlider}
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="open drawer"
+						sx={{ mr: 0 }}
+					>
+						<MenuIcon style={{ color: '#21325e' }} />
+					</IconButton>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+						style={{ color: '#21325e' }}
+					>
+						<Link to="/">
+							<img className="logo" src={logo} alt="logo" />
+						</Link>
+						{/* Easy Res */}
+					</Typography>
+					<Drawer open={open} anchor="left" onClose={toggleSlider}>
+						{SideList()}
+					</Drawer>
+					<Stack direction="row" spacing={2}>
+						{Auth.loggedIn() ? (
+							<>
+								<a href="/" onClick={logout}>
+									<Button
+										className="signBtn"
+										variant="outlined"
+										style={{ color: '#21325e' }}
+									>
+										Logout
+									</Button>
+								</a>
+							</>
+						) : (
+							<>
+								<Link to="/login" style={{ textDecoration: 'none' }}>
+									<Button
+										size="medium"
+										className="signBtn"
+										variant="outlined"
+										style={{ color: '#21325e' }}
+									>
+										Login
+									</Button>
+								</Link>
+							</>
+						)}
+					</Stack>
 
-          <Search sx={{ borderColor: '#21325e' }}>
-            <SearchIconWrapper>
-              <SearchIcon style={{ color: '#21325e' }} />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              style={{ color: '#21325e' }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+					<Search sx={{ borderColor: '#21325e' }}>
+						<SearchIconWrapper>
+							<SearchIcon style={{ color: '#21325e' }} />
+						</SearchIconWrapper>
+						<StyledInputBase
+							placeholder="Search…"
+							inputProps={{ 'aria-label': 'search' }}
+							style={{ color: '#21325e' }}
+						/>
+					</Search>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }
